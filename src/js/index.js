0ca597a07a45c29293875c11d4fbe55d0ef84eb1
Dvitash <dvitash3414@gmail.com>
2021-04-06 13:14:36 -0400

    some questions and stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
 console.log("connected");
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+const questionContainer = [
+    {
+        question: "Who is on the 20$ bill?",
+        answers:
+        {
+            a: "Frederick Douglass",
+            b: "Andrew Jackson",
+            c: "Barack Obama",
+            d: "Benjamin Franklin"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Who is on the penny?",
+        answers:
+        {
+            a: "Taylor Swift",
+            b: "Donald Trump",
+            c: "John Adams",
+            d: "Abraham Lincoln"
+        },
+        correctAnswer: "d"
+    }
+]
