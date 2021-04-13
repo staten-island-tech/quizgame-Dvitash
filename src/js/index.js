b6db700ae9da2389869132de873017ebeb039e24
Dvitash <dvitash3414@gmail.com>
2021-04-12 23:11:13 -0400

    lots of progress, basic gui and stuff. functions too

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 console.log("connected");
+
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+
+startButton.addEventListener('click', startGame);
+
+function startGame(){
+    console.log('started game');
+    startButton.classList.add('hide');
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
